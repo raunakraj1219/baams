a4e4a3d4a854c829ca5e4eac16296a9443fcc9cc
Raunak Raj <44779389+raunakraj1219@users.noreply.github.com>
2024-12-14 14:29:12 +0530

    console

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,9 @@ import { MENU_OPTIONS, PARENT_ROUTE, PATHS } from "../constants";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    const handleNavItemClick = () => {};
+    const handleNavItemClick = () => {
+        console.log("click");
+    };
     return (
         <nav className="flex p-5 flex-wrap justify-between items-center shadow-md border-b border-gray-200">
             <Link to={PARENT_ROUTE}>
